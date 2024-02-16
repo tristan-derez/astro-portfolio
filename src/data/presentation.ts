d336ec97a7209cfd90f8e279596008e214dcb648
Tristan Derez <tristan.derez@gmail.com>
2024-02-16 14:54:21 +0100

    Update description in presentation.ts

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -16,7 +16,7 @@ const presentation: Presentation = {
   title: "Hi, I'm Tristan 👋",
   // profile: "/profile.webp",
   description:
-    "a *french fullstack developer*. I mainly code in *TypeScript*, but I enjoy coding in Python, Go and c#. Most of the time, I use react for the frontend and node.js with adonis or hono for the backend. <br> I like learning new things, reading, watching movies and a lot of others random things",
+    "A *french fullstack developer*. I mainly code in *TypeScript*, but I enjoy coding in Python & Go. Most of the time, I use react for the frontend and node.js with adonis or hono for the backend. <br> I like learning new things, reading, watching movies and a lot of others random things",
   socials: [
     {
       label: "LinkedIn",
